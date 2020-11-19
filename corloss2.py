7cb8b90cea13552f155b4c382b58bee7328d020a
XiaoyuYu705 <74692226+XiaoyuYu705@users.noreply.github.com>
2020-11-19 15:54:50 +0800

    Update corloss2.py

--- corloss2.py
+++ corloss2.py
@@ -20,16 +20,11 @@ class Correlation_CrossEntropyLoss(nn.Module):
         inputs = input.data
      
         for i in range(inputs.shape[0]):
-            #print(inputs[i])
-            #print(target[i])
-            #print(inputs[i,target[i]])
             numerator = np.exp(inputs[i,target[i]])
             A = np.exp(inputs[i, :])
-            #print(A)
             denominator = A.sum(dim=0)
             target_prob = numerator / denominator
             index = torch.tensor([[target[i]]])
-            #print(index)
 
             Xid1 = X1.gather(1,index)
             Xid2 = X2.gather(1,index)#d=2
@@ -37,20 +32,20 @@ class Correlation_CrossEntropyLoss(nn.Module):
             Yid1 = Y1.gather(1,index)
             Yid1 = Yid1.view(1)
             Yid1 = Yid1.squeeze(0).long()
-            #print(Yid1)
+
             #Yid1.data = Yid1.data-1#ImageNet
-            #print(Yid1)
+
             Yid2 = Y2.gather(1,index)#d=2
             Yid2 = Yid2.view(1)#d=2
             Yid2 = Yid2.squeeze(0).long()#d=2
             #Yid2.data = Yid2.data-1#ImageNet
             
             cor_1 = inputs[i].gather(0,Yid1)#outputsd的值
-            #print(cor_1)
+
             Pid1 = np.exp(cor_1) / denominator
             cor_2 = inputs[i].gather(0,Yid2)#output的值,d=2
             Pid2 = np.exp(cor_2) / denominator#d=2
-            #print(Pid2)
+
         
             k = 0
             z = 0
@@ -59,7 +54,6 @@ class Correlation_CrossEntropyLoss(nn.Module):
             #if target_prob > (T * (Xid1 * Pid1)):#d=1
                 loss = -np.log(target_prob - T * ((Xid1 * Pid1)+(Xid2 * Pid2)))#d=2
                 #print(target_prob - T * ((Xid1 * Pid1)+(Xid2 * Pid2)))
-                #print(loss)
                 #loss = -np.log(target_prob - T * (Xid1 * Pid1))#d=1
                 if Pid1 != 0 or Pid2 != 0:#d=2
                 #if Pid1 != 0:#d=1
@@ -70,7 +64,7 @@ class Correlation_CrossEntropyLoss(nn.Module):
                     k = k+0
             else:
                 loss = -np.log(target_prob)
-                #print(loss)
+
                 j = j+1
             #print(loss)
                
